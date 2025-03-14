2ae8ead9bae27aa2ade6f1fa5018b3c63c4fe32f
Theo Paul Santana <theops2@gmail.com>
2025-03-14 16:27:18 +0100

    Complete repository reset with new files

--- LogSnap/Data/CoreData/Extensions/CoreDataImports.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-import CoreData
-import UIKit
-
-// This file ensures UIKit is available for Core Data transformable attributes like UIImage
-// The Core Data generated files need this import when using UIImage as a transformable attribute 
--- LogSnap/README_LOGO.md
+++
@@ -1,1 +0,0 @@
-// Create an image named "logo.png" and place it in the Assets.xcassets/logo.imageset folder for the app to use
